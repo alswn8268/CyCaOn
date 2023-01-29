288bb555deea1a7d553e2d876bf03899165ee086
alswn8268 <kamil1104@naver.com>
2023-01-29 09:39:07 +0900

    230129

---
+++ src/main/java/com/cyber/cafe/WebsocketHandler.java
@@ -0,0 +1,45 @@
+package com.cyber.cafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebsocketHandler extends TextWebSocketHandler{
+
+	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		
+		System.out.println("#ChattingHandler, afterConnectionEstablished");
+		sessionList.add(session);
+		
+		System.out.println(session.getPrincipal().getName() + "님이 입장하셨습니다.");
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		System.out.println("#ChattingHandler, handleMessage");
+		System.out.println(session.getId() + ": " + message);
+		
+		for(WebSocketSession s : sessionList) {
+			s.sendMessage(new TextMessage(session.getPrincipal().getName() + ":" + message.getPayload()));
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		
+		System.out.println("#ChattingHandler, afterConnectionClosed");
+
+		sessionList.remove(session);
+		
+		System.out.println(session.getPrincipal().getName() + "님이 퇴장하셨습니다.");
+	}
+	
+}
